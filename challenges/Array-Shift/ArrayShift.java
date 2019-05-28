435dc6b442ff23502a5a514cf7b505f37dd80011
Charles Clemens <cclemensjr@gmail.com>
2019-05-28 15:25:03 -0700

    Added loop to method

--- challenges/Array-Shift/ArrayShift.java
+++ challenges/Array-Shift/ArrayShift.java
@@ -1,11 +1,21 @@
 public class ArrayShift {
     public static void main(String[] args) {
-        insertShiftArray(null, 0);
+        int[] arr = { 0, 1, 2, 3 };
+        insertShiftArray(arr, 0);
     }
 
-    public static int[] insertShiftArray(int[] arr, int val) {
-        System.out.println("Inside insertShiftArray");
 
-        return null;
+    /***********
+     *  Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+     * */
+    public static int[] insertShiftArray(int[] array, int value) {
+        int length = array.length + 1;
+        int[] shiftedArray = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            System.out.println("Inside insertShiftArray");
+        }
+
+        return shiftedArray;
     }
 }
