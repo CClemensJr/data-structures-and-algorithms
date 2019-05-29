20f9f3ae7dfb19bfb5d7bd7e56e549b4d495ed8c
Charles Clemens <cclemensjr@gmail.com>
2019-05-29 13:36:27 -0700

    Propertly setup gradle

---
+++ challenges/arrayShift/src/test/java/arrayShift/ArrayShiftTest.java
@@ -0,0 +1,53 @@
+package arrayShift;
+
+import org.junit.Test;
+
+public class ArrayShiftTest {
+//    @Test
+//    public void testMethodReturnNotNull() {
+//        ArrayShift arrShift = new ArrayShift();
+//        int[] originalArray = { 1, 43, 17, 19 };
+//        int valueToBeInserted = 150;
+//
+//        int[] shiftedArray = arrShift.insertShiftArray(originalArray, valueToBeInserted);
+//
+//        assertNotNull(shiftedArray);
+//    }
+//
+//
+//    @Test
+//    public void testMethodReturnsArray() {
+//        ArrayShift arrShift = new ArrayShift();
+//        int[] originalArray = { 1, 43, 17, 19 };
+//        int valueToBeInserted = 150;
+//
+//        int[] shiftedArray = arrShift.insertShiftArray(originalArray, valueToBeInserted);
+//
+//        assertTrue(shiftedArray instanceof Array);
+//    }
+//
+//
+//    @Test
+//    public void testMethodReturnsLargerArray() {
+//        ArrayShift arrShift = new ArrayShift();
+//        int[] originalArray = { 1, 43, 17, 19 };
+//        int valueToBeInserted = 150;
+//
+//        int[] shiftedArray = arrShift.insertShiftArray(originalArray, valueToBeInserted);
+//
+//        assertTrue(shiftedArray.length == 5);
+//    }
+//
+//
+//    @Test
+//    public void testMethodReturnsShiftedArray() {
+//        ArrayShift arrShift = new ArrayShift();
+//        int[] originalArray = { 1, 43, 17, 19 };
+//        int valueToBeInserted = 150;
+//        int[] expectedArray = { 1, 43, 150, 17, 19 };
+//
+//        int[] shiftedArray = arrShift.insertShiftArray(originalArray, valueToBeInserted);
+//
+//        assertEquals(expectedArray, shiftedArray);
+//    }
+}
