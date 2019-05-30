7724296957a7320a8756b49a703c53261d30736f
Charles Clemens <cclemensjr@gmail.com>
2019-05-29 21:17:03 -0700

    Created failing test

--- challenges/arrayBinarySearch/src/test/java/arrayBinarySearch/BinarySearchTest.java
+++ challenges/arrayBinarySearch/src/test/java/arrayBinarySearch/BinarySearchTest.java
@@ -1,4 +1,17 @@
 package arrayBinarySearch;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class BinarySearchTest {
+    @Test
+    public void binarySearchShouldReturnTheCorrectIndex() {
+        BinarySearch test = new BinarySearch();
+
+        int[] arr = { 1, 25, 37, 75, 125, 148 };
+        int val = 37;
+        int expectedIndex = 2;
+
+        assertTrue(binarySearch(arr, val) == expectedIndex);
+    }
 }
