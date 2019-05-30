b2197fd36fccbeabae66cd74c8011668e76876fc
Charles Clemens <cclemensjr@gmail.com>
2019-05-29 22:30:45 -0700

    Added method logic

--- challenges/arrayBinarySearch/src/main/java/arrayBinarySearch/BinarySearch.java
+++ challenges/arrayBinarySearch/src/main/java/arrayBinarySearch/BinarySearch.java
@@ -2,8 +2,30 @@ package arrayBinarySearch;
 
 public class BinarySearch {
     public int binarySearch(int[] arr, int val) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = (start + end) / 2;
 
+        if (val == arr[mid]) {
+            return mid;
+        }
 
-        return 0;
+        while (val != arr[mid]) {
+            if (val > arr[mid]) {
+                start = mid;
+                mid = (start + end) / 2;
+
+                continue;
+            }
+
+            if (val < arr[mid]) {
+                end = mid;
+                mid = (start + end) / 2;
+
+                continue;
+            }
+        }
+
+        return mid;
     }
 }
