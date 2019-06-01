0f6ab3acc2c53879c22669bdffe416db638b9f41
Charles Clemens <cclemensjr@gmail.com>
2019-06-01 15:27:05 -0700

    created failing test

--- data-structures/linkedList/src/test/java/linkedList/LinkedListTest.java
+++ data-structures/linkedList/src/test/java/linkedList/LinkedListTest.java
@@ -1,4 +1,25 @@
 package linkedList;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class LinkedListTest {
+    @Test
+    public void canSuccessfullyInstantiateAnEmptyLinkedList() {
+        LinkedList test = new LinkedList();
+
+        assertTrue(test.size == 0);
+    }
+
+    // Can properly insert into the linked list
+
+    // The head property will properly point to the first node in the linked list
+
+    // Can properly insert multiple nodes into the linked list
+
+    // Will return true when finding a value within the linked list that exists
+
+    // Will return false when searching for a value in the linked list that does not exist
+
+    // Can properly return a collection of all the values that exist in the linked list
 }
