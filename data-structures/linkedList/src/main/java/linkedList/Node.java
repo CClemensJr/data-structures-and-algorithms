e49ff0329bf24bf9903e234b143a12983630c380
Charles Clemens <cclemensjr@gmail.com>
2019-06-01 14:15:21 -0700

    Added getters and setters to Node class

--- data-structures/linkedList/src/main/java/linkedList/Node.java
+++ data-structures/linkedList/src/main/java/linkedList/Node.java
@@ -1,12 +1,21 @@
 package linkedList;
 
 public class Node {
+    // Instance Variables
     private int value;
-    public Node next;
+    private Node next;
 
+    // Constructors
     public Node() {}
 
     public Node(int v) {
         this.value = v;
     }
+
+    // Getter and Setter methods
+    public int getValue() { return value; }
+    public Node getNext() { return next; }
+
+    public void setValue() { this.value = value; }
+    public void setNext() { this.next = next; }
 }
