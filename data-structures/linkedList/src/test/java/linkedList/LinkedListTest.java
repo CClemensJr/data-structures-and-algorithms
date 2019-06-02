960e91a1ef5fddf52a6e5ba99593d941579bcb79
Charles Clemens <cclemensjr@gmail.com>
2019-06-01 22:51:31 -0700

    Passed test

--- data-structures/linkedList/src/test/java/linkedList/LinkedListTest.java
+++ data-structures/linkedList/src/test/java/linkedList/LinkedListTest.java
@@ -80,7 +80,7 @@ public class LinkedListTest {
             test.insert(i);
         }
 
-        int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] expected = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
         int[] actual = test.print();
 
         assertArrayEquals(expected, actual);
