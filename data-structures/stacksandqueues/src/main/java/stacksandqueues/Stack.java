8e725e0a70b31fed783c88c60939c616bfc7b9e1
Charles Clemens <cclemensjr@gmail.com>
2019-06-10 10:07:51 -0700

    Added properties, constructors, getters/setters

--- data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
+++ data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
@@ -1,4 +1,28 @@
 package stacksandqueues;
 
 public class Stack {
+/************
+ * Instance Variables
+ * */
+    private Node top;
+
+
+/************
+ * Constructors
+ * */
+    public Stack(Node t) { this.top = t; }
+
+
+/************
+ * Getters/Setters
+ * */
+    public Node getTop() { return this.top; }
+
+    public void setTop(Node t) { this.top = t; }
+
+
+/************
+ * Additional Instance Methods
+ * */
+
 }
