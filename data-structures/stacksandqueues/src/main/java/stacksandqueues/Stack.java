074698794f51361affae837d4c188cd6a7313b9b
Charles Clemens <cclemensjr@gmail.com>
2019-06-10 10:26:56 -0700

    Fixed size tracking logic

--- data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
+++ data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
@@ -24,7 +24,7 @@ public class Stack {
     public int getSize() { return this.size; }
 
     public void setTop(Node t) { this.top = t; }
-    public void setSize() { this.size++; }
+    public void setSize(int s) { this.size = (s == 1) ? this.size + 1 : this.size - 1; }
 
 
 /************
@@ -32,8 +32,16 @@ public class Stack {
  * */
     // The push method takes any value as an argument and adds a new node with that value to the top of the stack with
     // an O(1) Time performance.
-    public void push(Object value) { this.top = new Node(value); }
+    public void push(Object value) {
+        setTop(new Node(value));
+        setSize();
+    }
 
 
+    // The push method does not take any argument, removes the node from the top of the stack, and returns the node’s value.
+    public Object pop() {
+
+
+    }
 
 }
