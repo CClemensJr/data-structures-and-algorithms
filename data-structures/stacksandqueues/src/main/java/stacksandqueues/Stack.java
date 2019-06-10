d9ccdbfbd45ac3171f5cbac873aa5bc7347f03b5
Charles Clemens <cclemensjr@gmail.com>
2019-06-10 10:15:53 -0700

    Added size tracking

--- data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
+++ data-structures/stacksandqueues/src/main/java/stacksandqueues/Stack.java
@@ -5,24 +5,33 @@ public class Stack {
  * Instance Variables
  * */
     private Node top;
+    private int size;
 
 
 /************
  * Constructors
  * */
-    public Stack(Node t) { this.top = t; }
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
 
 
 /************
  * Getters/Setters
  * */
     public Node getTop() { return this.top; }
+    public int getSize() { return this.size; }
 
     public void setTop(Node t) { this.top = t; }
+    public void setSize() { this.size++; }
 
 
 /************
  * Additional Instance Methods
  * */
+    public void push(Object value) {
+
+    }
 
 }
