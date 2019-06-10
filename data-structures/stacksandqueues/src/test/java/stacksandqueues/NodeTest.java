d85422914d4bf5289addf9dcf79cd505e153efa9
Charles Clemens <cclemensjr@gmail.com>
2019-06-10 10:43:09 -0700

    Passed node tests

--- data-structures/stacksandqueues/src/test/java/stacksandqueues/NodeTest.java
+++ data-structures/stacksandqueues/src/test/java/stacksandqueues/NodeTest.java
@@ -1,4 +1,29 @@
 package stacksandqueues;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class NodeTest {
+    @Test
+    public void canCreateNode() {
+        Node testNode = new Node("7");
+
+        assertTrue(testNode.getValue() == "7");
+    }
+
+    @Test
+    public void canGetNodeValue() {
+        Node testNode = new Node("7");
+
+        assertTrue(testNode.getValue() == "7");
+    }
+
+    @Test
+    public void canSetNodeValue() {
+        Node testNode = new Node("7");
+
+        testNode.setValue("77");
+
+        assertTrue(testNode.getValue() == "77");
+    }
 }
