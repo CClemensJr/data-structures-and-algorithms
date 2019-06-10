6cee3b94dce38988bc63ceab1e918f745c3c3032
Charles Clemens <cclemensjr@gmail.com>
2019-06-10 11:15:46 -0700

    Added queue properties, constructors, and getters/setters

--- data-structures/stacksandqueues/src/main/java/stacksandqueues/Queue.java
+++ data-structures/stacksandqueues/src/main/java/stacksandqueues/Queue.java
@@ -1,4 +1,37 @@
 package stacksandqueues;
 
 public class Queue {
+/************
+ * Instance Variables
+ * */
+    private Node front;
+    private Node rear;
+    private int size;
+
+
+/************
+ * Constructors
+ * */
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+
+/************
+ * Getters/Setters
+ * */
+    public Node getFront() { return this.front; }
+    public Node getRear() { return this.rear; }
+    public int getSize() { return this.size; }
+
+    public void setFront(Node f) { this.front = f; }
+    public void setRear(Node r) { this.front = r; }
+    public void setSize(int s) { this.size = (s == 1) ? this.size + 1 : this.size - 1; }
+
+
+/************
+ * Additional Instance Methods
+ * */
 }
