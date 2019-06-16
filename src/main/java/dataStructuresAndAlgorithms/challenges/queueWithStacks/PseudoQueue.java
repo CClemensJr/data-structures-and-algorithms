fabc01d750353a33611bbe23303c6e90d029d34d
Charles Clemens <cclemensjr@gmail.com>
2019-06-16 13:23:06 -0700

    Added instance vars and methods

--- src/main/java/dataStructuresAndAlgorithms/challenges/queueWithStacks/PseudoQueue.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/queueWithStacks/PseudoQueue.java
@@ -1,4 +1,29 @@
 package dataStructuresAndAlgorithms.challenges.queueWithStacks;
 
+import dataStructuresAndAlgorithms.dataStructures.stacksAndQueues.Stack;
+
 public class PseudoQueue {
+/***********
+ * Instance Variables
+ * */
+    protected Stack enqueStack;
+    protected Stack dequeStack;
+
+
+/***********
+ * Constructor
+ * */
+    public PseudoQueue() {}
+
+
+/***********
+ * Instance Methods
+ * */
+    public void enqueue(Object value) {
+
+    }
+
+    public Object dequeue() {
+        return null;
+    }
 }
