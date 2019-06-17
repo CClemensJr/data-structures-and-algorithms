e211713e4250ce3cd4e4f98c641b1e80497ce64f
Charles Clemens <cclemensjr@gmail.com>
2019-06-17 10:12:25 -0700

    Added properties and methods to node class

--- src/main/java/dataStructuresAndAlgorithms/dataStructures/tree/Node.java
+++ src/main/java/dataStructuresAndAlgorithms/dataStructures/tree/Node.java
@@ -1,4 +1,26 @@
 package dataStructuresAndAlgorithms.dataStructures.tree;
 
-public class Node {
+public class Node<T> {
+/***************
+ * Instance Variables
+ * */
+    private T value;
+    private Node leftChild;
+    private Node rightChild;
+
+
+/***************
+ * Constructors
+ * */
+    public Node(T value) {
+        this.value = value;
+    }
+
+
+/***************
+ * Getters / Setters
+ * */
+    public T getValue() { return this.value; }
+    public Node getLeftChild() { return this.leftChild; }
+    public Node getRightChild() { return this.rightChild; }
 }
