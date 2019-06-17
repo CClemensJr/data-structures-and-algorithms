bf3d4195c56ad7773159f00210b15e142b56c60b
Charles Clemens <cclemensjr@gmail.com>
2019-06-17 10:20:45 -0700

    Added properties and methods to tree class

--- src/main/java/dataStructuresAndAlgorithms/dataStructures/tree/Tree.java
+++ src/main/java/dataStructuresAndAlgorithms/dataStructures/tree/Tree.java
@@ -1,4 +1,24 @@
 package dataStructuresAndAlgorithms.dataStructures.tree;
 
-public class Tree {
+public class Tree<T> {
+/***************
+ * Instance Variables
+ * */
+    private Node root;
+
+
+/***************
+ * Constructors
+ * */
+    public Tree() {}
+
+    public Tree(T value) {
+        this.root = new Node(value);
+    }
+
+
+/***************
+ * Getters / Setters
+ * */
+    public Node getRoot() { return this.root; }
 }
