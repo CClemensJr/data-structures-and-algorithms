921667f6a6af040dc75f246661debc71b239ee3d
Charles Clemens <cclemensjr@gmail.com>
2019-06-18 13:29:43 -0700

    Added fizzBuzzTree method

--- src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
@@ -1,4 +1,14 @@
 package dataStructuresAndAlgorithms.challenges.fizzBuzzTree;
 
-public class FizzBuzzTree {
+import dataStructuresAndAlgorithms.dataStructures.tree.BinaryTree;
+
+import java.util.ArrayList;
+
+public class FizzBuzzTree<T> {
+
+    public BinaryTree fizzBuzzTree(BinaryTree tree) {
+        ArrayList<T> nodes = tree.preOrder(tree.getRoot());
+
+        return null;
+    }
 }
