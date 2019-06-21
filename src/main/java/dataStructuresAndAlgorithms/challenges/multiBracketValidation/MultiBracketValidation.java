a6d0aeec3365b65df17ccf55ab23fa19e48dbb36
Charles Clemens <cclemensjr@gmail.com>
2019-06-21 10:19:17 -0700

    Implemented multibracketvalidation method

--- src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
@@ -11,8 +11,28 @@ public class MultiBracketValidation {
             if (input.length() % 2 != 0) {
                 return false;
             }
-//            Stack brackets = new Stack();
-//            ArrayList<char> chars = input.split("");
+
+            String[] chars = input.split("");
+            Stack brackets = new Stack();
+
+            for (int i = 0; i < input.length(); i++) {
+                if (chars[i] == "{" || chars[i] == "(" || chars[i] == "["  ) {
+                    brackets.push(chars[i]);
+                } else if (chars[i] == "}" || chars[i] == ")" || chars[i] == "]") {
+                    if (brackets.getSize() == 0) {
+                        return false;
+                    } else {
+                        if (chars[i] == "{" && brackets.peek() == "}" ||
+                            chars[i] == "(" && brackets.peek() == ")" ||
+                            chars[i] == "[" && brackets.peek() == "]") {
+                            brackets.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+
+            }
         } catch (Exception e) {
             System.out.println("An error has occurred: " + e);
         }
--- src/test/java/dataStructuresAndAlgorithms/MultiBracketValidationTest.java
+++ src/test/java/dataStructuresAndAlgorithms/MultiBracketValidationTest.java
@@ -1,4 +1,16 @@
 package dataStructuresAndAlgorithms;
 
+import static dataStructuresAndAlgorithms.challenges.multiBracketValidation.MultiBracketValidation.multiBracketValidation;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class MultiBracketValidationTest {
+    @Test
+    public void canReturnFalseIfMissingABracket() {
+        String test = "{}[](";
+
+        assertFalse(multiBracketValidation(test));
+    }
+
+
 }
