50cb862c2a68b826f9ecece7d18bb0e38f05949e
Charles Clemens <cclemensjr@gmail.com>
2019-06-20 21:50:00 -0700

    Created animal pojo

---
+++ src/main/java/dataStructuresAndAlgorithms/challenges/fifoAnimalShelter/Animal.java
@@ -0,0 +1,9 @@
+package dataStructuresAndAlgorithms.challenges.fifoAnimalShelter;
+
+public class Animal {
+    String typeOfAnimal;
+
+    public Animal(String typeOfAnimal) { this.typeOfAnimal = typeOfAnimal; }
+
+    public String getTypeOfAnimal() {  return typeOfAnimal; }
+}
