56096f508d09c064367a60b2da7f99c68b2391fa
Charles Clemens <cclemensjr@gmail.com>
2019-06-21 11:02:41 -0700

    Added fizzbuzztree method

--- src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class FizzBuzzTree<T> {
 
     public BinaryTree fizzBuzzTree(BinaryTree tree) {
-        List<T> nodes = tree.preOrder(tree.getRoot());
+        try {
+            List<T> nodes = tree.preOrder(tree.getRoot());
+
+            for (T node : nodes) {
+                System.out.println(node);
+            }
+        } catch (Exception e) {
+            System.out.println("An error has occurred: " + e);
+        }
 
         return null;
     }
--- src/test/java/dataStructuresAndAlgorithms/FizzBuzzTreeTest.java
+++ src/test/java/dataStructuresAndAlgorithms/FizzBuzzTreeTest.java
@@ -1,4 +1,8 @@
 package dataStructuresAndAlgorithms;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FizzBuzzTreeTest {
+
 }
