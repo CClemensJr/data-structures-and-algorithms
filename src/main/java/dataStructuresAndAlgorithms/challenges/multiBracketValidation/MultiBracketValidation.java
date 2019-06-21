f312837500219da6a4d24e4936ebb0d9b047b2dd
Charles Clemens <cclemensjr@gmail.com>
2019-06-21 09:48:23 -0700

    Created method signature

--- src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
@@ -1,5 +1,7 @@
 package dataStructuresAndAlgorithms.challenges.multiBracketValidation;
 
 public class MultiBracketValidation {
-
+    public static boolean multiBracketValidation(String input) {
+        return false;
+    }
 }
