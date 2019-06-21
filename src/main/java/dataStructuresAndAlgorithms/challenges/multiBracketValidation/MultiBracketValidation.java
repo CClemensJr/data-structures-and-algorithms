32d86e534d066da6e080391b83a24b4a7a28f4a1
Charles Clemens <cclemensjr@gmail.com>
2019-06-21 09:59:54 -0700

    Created test file

--- src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/multiBracketValidation/MultiBracketValidation.java
@@ -1,7 +1,21 @@
 package dataStructuresAndAlgorithms.challenges.multiBracketValidation;
 
+import dataStructuresAndAlgorithms.dataStructures.stacksAndQueues.Stack;
+
+import java.util.ArrayList;
+
 public class MultiBracketValidation {
+
     public static boolean multiBracketValidation(String input) {
+        try {
+            if (input.length() % 2 != 0) {
+                return false;
+            }
+//            Stack brackets = new Stack();
+//            ArrayList<char> chars = input.split("");
+        } catch (Exception e) {
+            System.out.println("An error has occurred: " + e);
+        }
         return false;
     }
 }
