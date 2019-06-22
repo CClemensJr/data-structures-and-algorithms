256201f5495d51682f1cff4edaff4533e12330cd
Charles Clemens <cclemensjr@gmail.com>
2019-06-22 10:43:09 -0700

    Added setter to node

--- src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/fizzBuzzTree/FizzBuzzTree.java
@@ -1,21 +1,58 @@
 package dataStructuresAndAlgorithms.challenges.fizzBuzzTree;
 
-import dataStructuresAndAlgorithms.dataStructures.tree.BinaryTree;
+import dataStructuresAndAlgorithms.dataStructures.tree.*;
 import java.util.List;
+
 
 public class FizzBuzzTree<T> {
 
-    public BinaryTree fizzBuzzTree(BinaryTree tree) {
+    public static void fizzBuzzTree(BinaryTree tree) {
         try {
-            List<T> nodes = tree.preOrder(tree.getRoot());
+//            List nodes = tree.preOrder(tree.getRoot());
+//            BinaryTree newTree = new BinaryTree();
+//
+//            for (Object node : nodes) {
+//                if ((int) node % 5 == 0 && (int) node % 3 == 0 ) { node = "FizzBuzz"; }
+//                if ((int) node % 3 == 0) { node = "Fizz"; }
+//                if ((int) node % 5 == 0) { node = "Buzz"; }
+//
+//                newTree.addNode(node);
+//            }
+//
+//            return newTree;
 
-            for (T node : nodes) {
-                System.out.println(node);
-            }
+            // If the tree isn't null, send the root node to fizzbuzznode
+            if (tree != null) fizzBuzzNode(tree.getRoot());
         } catch (Exception e) {
             System.out.println("An error has occurred: " + e);
         }
 
-        return null;
+//        return null;
+    }
+
+
+    public static void fizzBuzzNode(Node root) {
+        // if root isn't null
+        if(root != null) {
+            // Set the value of root equal to the value returned from the fizzBuzz method
+            root.setValue(fizzBuzz(root.getValue()));
+            // Then use recursion to continue traversing down the tree
+            fizzBuzzNode(root.getLeftChild());
+            fizzBuzzNode(root.getRightChild());
+        }
+    }
+
+
+    public static Object fizzBuzz(Object value) {
+        try {
+            if ((int) value % 5 == 0 && (int) value % 3 == 0 ) { value = "FizzBuzz"; }
+            else if ((int) value % 3 == 0) { value = "Fizz"; }
+            else if ((int) value % 5 == 0) { value = "Buzz"; }
+
+            return value;
+
+        } catch (Exception e) {
+            System.out.println("An error has occurred: " + e);
+        }
     }
 }
--- src/test/java/dataStructuresAndAlgorithms/FizzBuzzTreeTest.java
+++ src/test/java/dataStructuresAndAlgorithms/FizzBuzzTreeTest.java
@@ -1,8 +1,19 @@
 package dataStructuresAndAlgorithms;
 
+import dataStructuresAndAlgorithms.dataStructures.tree.BinaryTree;
 import org.junit.Test;
+
+import static dataStructuresAndAlgorithms.challenges.fizzBuzzTree.FizzBuzzTree.fizzBuzzTree;
 import static org.junit.Assert.*;
 
 public class FizzBuzzTreeTest {
+    @Test
+    public void canUpdateNodeValueToFizz() {
+        BinaryTree testTree = new BinaryTree(3);
 
+        BinaryTree actual = fizzBuzzTree(testTree);
+        BinaryTree expected = new BinaryTree("Fizz");
+
+        assertEquals(expected.getRoot(), actual.getRoot());
+    }
 }
