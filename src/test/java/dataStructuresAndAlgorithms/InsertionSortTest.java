ab001d44db9af5c958d485a7b8e0712243a50a75
Charles Clemens <cclemensjr@gmail.com>
2019-07-01 13:15:13 -0700

    Tested insertion sort works

--- src/test/java/dataStructuresAndAlgorithms/InsertionSortTest.java
+++
@@ -1,4 +0,0 @@
-package dataStructuresAndAlgorithms;
-
-public class InsertionSortTest {
-}
---
+++ src/test/java/dataStructuresAndAlgorithms/SortTest.java
@@ -0,0 +1,17 @@
+package dataStructuresAndAlgorithms;
+
+import org.junit.Test;
+
+import static dataStructuresAndAlgorithms.dataStructures.sorts.Sort.insertionSort;
+import static org.junit.Assert.*;
+
+public class SortTest {
+    @Test
+    public void canUseInsertionSort() {
+        int[] test = { 12, 11, 13, 5, 6 };
+        int[] expected = { 5, 6, 11, 12, 13 };
+        int[] actual = insertionSort(test);
+
+        assertArrayEquals(expected, actual);
+    }
+}
