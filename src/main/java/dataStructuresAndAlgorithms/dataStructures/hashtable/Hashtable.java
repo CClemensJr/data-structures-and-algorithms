e54a60a47b34e4092e7aff019bba3e8b86d23612
Charles Clemens <cclemensjr@gmail.com>
2019-07-08 21:20:35 -0700

    Added fields to hashmap

--- src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Hashtable.java
+++ src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Hashtable.java
@@ -1,4 +1,28 @@
 package dataStructuresAndAlgorithms.dataStructures.hashtable;
 
 public class Hashtable {
+/****************
+ * Instance Variables
+ * */
+    private Node[] map;
+
+
+/****************
+ * Constructors
+ * */
+    public Hashtable (int size) {
+        this.map = new Node[size];
+    }
+
+
+/****************
+ * Setters/Getters
+ * */
+    public Node[] getMap() { return this.map; }
+
+
+/****************
+ * Instance Methods
+ * */
+
 }
