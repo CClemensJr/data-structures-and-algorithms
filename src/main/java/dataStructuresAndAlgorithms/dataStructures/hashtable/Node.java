b6cae085e6d49e02301f6154148b8130f64a8df6
Charles Clemens <cclemensjr@gmail.com>
2019-07-08 21:08:32 -0700

    Set fields in Node class

--- src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Node.java
+++ src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Node.java
@@ -1,4 +1,34 @@
 package dataStructuresAndAlgorithms.dataStructures.hashtable;
 
 public class Node {
+/****************
+ * Instance Variables
+ * */
+    private String key;
+    private String value;
+    private Node next;
+
+
+/****************
+ * Constructors
+ * */
+    public Node () {}
+
+    public Node (String key, String value) {
+        this.key = key;
+        this.key = value;
+    }
+
+
+/****************
+ * Getters/Setters
+ * */
+    public String getKey() { return key; }
+    public void setKey(String key) { this.key = key; }
+
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+
+    public Node getNext() { return next; }
+    public void setNext(Node next) { this.next = next; }
 }
