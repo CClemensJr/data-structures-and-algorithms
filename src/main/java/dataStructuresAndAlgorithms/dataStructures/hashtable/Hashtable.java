ac2d8e15fa851952c7333db80eac158346b8421b
Charles Clemens <cclemensjr@gmail.com>
2019-07-08 22:01:53 -0700

    Tested that hashing works

--- src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Hashtable.java
+++ src/main/java/dataStructuresAndAlgorithms/dataStructures/hashtable/Hashtable.java
@@ -42,6 +42,21 @@ public class Hashtable {
 
     // hash: takes in an arbitrary key and returns an index in the collection.
     public int hash (String key) {
+        try {
+            int index = 0;
+            char[] keyCharacters = key.toCharArray();
+
+            for (int i = 0; i < this.map.length; i++) {
+                index += keyCharacters[i];
+            }
+
+            index = (index * 2048) % this.map.length;
+
+            return index;
+        } catch (Exception err) {
+
+            System.out.println("An error has occurred: " + err);
+        }
         return 0;
     }
 }
---
+++ src/test/java/dataStructuresAndAlgorithms/HashtableTest.java
@@ -0,0 +1,16 @@
+package dataStructuresAndAlgorithms;
+
+import dataStructuresAndAlgorithms.dataStructures.hashtable.Hashtable;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HashtableTest {
+    @Test
+    public void canHashAKey() {
+        Hashtable testTable = new Hashtable(5);
+        String testKey = "Test";
+        int testIndex = testTable.hash(testKey);
+
+        assertTrue(testIndex >= 0 && testIndex <= 4);
+    }
+}
