91c27d2523aa8cd038c305cb9e80ad87ab388030
Charles Clemens <cclemensjr@gmail.com>
2019-07-10 10:31:11 -0700

    Converted solution to hashmap

--- src/main/java/dataStructuresAndAlgorithms/challenges/repeatedWord/RepeatedWord.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/repeatedWord/RepeatedWord.java
@@ -1,22 +1,30 @@
 package dataStructuresAndAlgorithms.challenges.repeatedWord;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RepeatedWord {
     public static String findTheRepeatedWord(String string) {
-        // This code inspired by https://stackoverflow.com/questions/7488643/how-to-convert-comma-separated-string-to-arraylist
-        String[] originalWords = string.split("\\s*,\\s*");
+        try {
+            // This code inspired by https://stackoverflow.com/questions/7488643/how-to-convert-comma-separated-string-to-arraylist
+            String[] originalWords = string.split("\\s*,\\s*");
 
-        List<String> uniqueWords = new ArrayList<>();
+            //This code inspired by https://stackoverflow.com/questions/4820716/finding-repeated-words-on-a-string-and-counting-the-repetitions
+            Map<String, Integer> repeats = new HashMap<>();
 
-        for (String word : originalWords) {
-            if (!uniqueWords.contains(word)) {
-                uniqueWords.add(word);
-            } else {
-                return word;
+            for (String word : originalWords) {
+                Integer count = repeats.get(word);
+
+                if (count == null) count = 0;
+
+                repeats.put(word, count++);
             }
+
+            for (String word : repeats.keySet()) {
+                return (repeats.get(word) > 1) ? word : "There were no repeated words.";
+            }
+        } catch (Exception error) {
+            System.out.println("An error has occurred: " + error);
         }
 
         return null;
