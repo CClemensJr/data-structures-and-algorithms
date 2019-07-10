7a3a7dba2f2db4fe96d3f7d570838c37827bac0e
Charles Clemens <cclemensjr@gmail.com>
2019-07-10 11:02:58 -0700

    Fixed counter bug

--- src/main/java/dataStructuresAndAlgorithms/challenges/repeatedWord/RepeatedWord.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/repeatedWord/RepeatedWord.java
@@ -7,7 +7,7 @@ public class RepeatedWord {
     public static String findTheRepeatedWord(String string) {
         try {
             // This code inspired by https://stackoverflow.com/questions/7488643/how-to-convert-comma-separated-string-to-arraylist
-            String[] originalWords = string.split("\\s*,\\s*");
+            String[] originalWords = string.toLowerCase().split(" ");
 
             //This code inspired by https://stackoverflow.com/questions/4820716/finding-repeated-words-on-a-string-and-counting-the-repetitions
             Map<String, Integer> repeats = new HashMap<>();
@@ -17,11 +17,12 @@ public class RepeatedWord {
 
                 if (count == null) count = 0;
 
-                repeats.put(word, count++);
+                repeats.put(word, ++count);
             }
 
             for (String word : repeats.keySet()) {
-                return (repeats.get(word) > 1) ? word : "There were no repeated words.";
+                if (repeats.get(word) > 1) return word;
+
             }
         } catch (Exception error) {
             System.out.println("An error has occurred: " + error);
