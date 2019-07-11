1f8b4bab6f8c8867121dfabc2534747dc4d12cd5
Charles Clemens <cclemensjr@gmail.com>
2019-07-11 11:26:27 -0700

    Created method signature

--- src/main/java/dataStructuresAndAlgorithms/challenges/treeIntersection/TreeIntersection.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/treeIntersection/TreeIntersection.java
@@ -1,4 +1,10 @@
 package dataStructuresAndAlgorithms.challenges.treeIntersection;
 
+import dataStructuresAndAlgorithms.dataStructures.hashtable.Hashtable;
+import dataStructuresAndAlgorithms.dataStructures.tree.BinaryTree;
+
 public class TreeIntersection {
+    public static Hashtable treeIntersection(BinaryTree tree1, BinaryTree tree2) {
+        return null;
+    }
 }
