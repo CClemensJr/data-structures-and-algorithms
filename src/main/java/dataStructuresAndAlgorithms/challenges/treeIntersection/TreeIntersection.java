bce24dc0a1434644636559adb234a100acabb80d
Charles Clemens <cclemensjr@gmail.com>
2019-07-11 11:50:58 -0700

    Completed tree intersection method

--- src/main/java/dataStructuresAndAlgorithms/challenges/treeIntersection/TreeIntersection.java
+++ src/main/java/dataStructuresAndAlgorithms/challenges/treeIntersection/TreeIntersection.java
@@ -3,8 +3,21 @@ package dataStructuresAndAlgorithms.challenges.treeIntersection;
 import dataStructuresAndAlgorithms.dataStructures.hashtable.Hashtable;
 import dataStructuresAndAlgorithms.dataStructures.tree.BinaryTree;
 
-public class TreeIntersection {
-    public static Hashtable treeIntersection(BinaryTree tree1, BinaryTree tree2) {
-        return null;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeIntersection<T> {
+    public List<T> treeIntersection(BinaryTree tree1, BinaryTree tree2) {
+        List<T> tree1Values = tree1.inOrder(tree1.getRoot());
+        List<T> tree2Values = tree2.inOrder(tree2.getRoot());
+        List<T> commonValues = new ArrayList<>();
+
+        int maxLength = (tree1Values.size() > tree2Values.size()) ? tree1Values.size() : tree2Values.size();
+
+        for (int i = 0; i < maxLength; i++) {
+            if (tree1Values.get(i) == tree2Values.get(i)) commonValues.add(tree1Values.get(i));
+        }
+
+        return commonValues;
     }
 }
